9344fe543d7d2df44a1b9aa47d1c8bb42ad3ad86
c032 <git@c032.dev>
2024-02-19 16:23:54 +0000

    Remove `app.*` except `app.module.ts`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from "@nestjs/common";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-
-import { ConfigModule } from "./config/config.module";
-import { PostgresqlModule } from "./database/postgresql.module";
-import { PgClientService } from "./database/pgclient.service";
 import { FfxivModule } from "./ffxiv/ffxiv.module";
 
 @Module({
-  imports: [ConfigModule, PostgresqlModule, FfxivModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FfxivModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
