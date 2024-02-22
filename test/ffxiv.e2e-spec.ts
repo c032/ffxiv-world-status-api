618b4667df4a0e2b06547dc0b90869fdf1f45f33
c032 <git@c032.dev>
2024-02-22 12:58:40 +0000

    Add simple test

--- test/ffxiv.e2e-spec.ts
+++ test/ffxiv.e2e-spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
+
+import { Pool } from "../src/database/types/pool";
+
 import { AppModule } from "./../src/app.module";
 
-describe("AppController (e2e)", () => {
+describe("FfxivController (e2e)", () => {
   let app: INestApplication;
+  let pool: Pool;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +16,20 @@ describe("AppController (e2e)", () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
     await app.init();
+
+    pool = moduleFixture.get("PG_POOL");
   });
 
-  it("/ (GET)", () => {
+  afterEach(async () => {
+    await pool.end();
+  });
+
+  it("/ffxiv/worlds (GET)", () => {
     return request(app.getHttpServer())
-      .get("/")
+      .get("/ffxiv/worlds")
       .expect(200)
-      .expect("Hello World!");
+      .expect({ worlds: [] });
   });
 });
