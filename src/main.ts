83f8dffca341a51cd043e27ba5163bc0d0bac0c0
c032 <git@c032.dev>
2024-02-29 18:15:21 +0000

    Add CORS, kinda

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const corsOptions = {
+    // TODO: Get from config.
+    origin: "https://c032.dev",
+  };
+  app.enableCors(corsOptions);
+
   // TODO: Cleanup.
   const port = parseInt(process.env.PORT || "", 10);
   if (port.toString() !== process.env.PORT || port === 0) {
